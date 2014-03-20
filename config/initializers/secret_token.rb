ec689593b30530cbe895999c07a22fcfb69081c4
Stanley Santacruz <walter_07_8@hotmail.com>
2014-03-20 15:30:37 -0600

    commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebStan::Application.config.secret_key_base = 'b01e0a80424e182755c763d88e0777fceb12f03842a9d5703a0d3bb68ccf297c31ab15de5d7c76186ced0dd48869ce9c08ed5fc534e24e361afaa79d2fad6ff0'
